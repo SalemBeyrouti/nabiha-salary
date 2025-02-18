ab242a6bc2e2516b4101a17fdb78341e95395b17
salembeyrouti <salem_beyrouti11@hotmail.com>
2025-02-18 19:23:56 +0200

    changed definition of percentages

---
+++ nabiha.py
@@ -0,0 +1,9 @@
+salary = float(input("Enter your salary for the month: "))
+
+month = input("Enter the name of the month: ")
+
+savings = float(input("Enter the percentage of savings: ")) # type: ignore
+
+rent = float(input("Enter the percentage of rent: "))
+
+electricity = float(input("Enter the percentage of electricity: "))
