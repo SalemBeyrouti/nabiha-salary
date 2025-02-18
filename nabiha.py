5907afa6cbb345c1c6bf864d8f9485eb9d113406
salembeyrouti <salem_beyrouti11@hotmail.com>
2025-02-18 19:46:55 +0200

    added all the print commands

--- nabiha.py
+++ nabiha.py
@@ -2,7 +2,7 @@ salary = float(input("Enter your salary for the month: "))
 
 month = input("Enter the name of the month: ")
 
-savings = float(input("Enter the percentage of savings: ")) # type: ignore
+savings = float(input("Enter the percentage of savings: ")) 
 
 rent = float(input("Enter the percentage of rent: "))
 
@@ -23,3 +23,12 @@ yearlyrent = totalrent*12
 yearlyelectricity = totalelectricity*12
 
 salarysquared = salary*salary
+
+print("The amount allocated to savings: ", totalsavings  )
+print("The amount allocated to electricity: ", totalelectricity)
+print("The amount allocated to rent: ", totalrent)
+print("The total amount spent on savings, rent and electricity: ", totalspending)
+print("The remainder salary: ", remaindersalary)
+print("Yearly rent: ", yearlyrent)
+print("Yearly electricity: ", yearlyelectricity)
+print("Salary squared just for fun: ", salarysquared)
